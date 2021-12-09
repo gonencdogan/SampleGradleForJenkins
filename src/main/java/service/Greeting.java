2112facec4d0e452122fcfb2b8376c8aea4df547
gdogan <g.dogan@form-solutions.de>
2021-12-09 17:53:17 +0100

    added checkstyle plugin

--- src/main/java/service/Greeting.java
+++ src/main/java/service/Greeting.java
@@ -2,6 +2,11 @@ package service;
 
 public class Greeting {
 
+	/**
+	 *
+	 * @param name
+	 * @return
+	 */
 	public String greet(String name) {
 		String unusedVariable = "Gonenc";
 		return String.format("Hello %s!", name);
